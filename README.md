6a28f9fc5a22954469c18e245d4c830e9242e94a
김우빈 <kwb020312@naver.com>
2023-10-08 00:17:48 +0900

    Feat: Measuring The Elongation

--- README.md
+++ README.md
@@ -18,3 +18,11 @@ cd node && npm install
 ```
 
 ---
+
+### 😂Minimum Rectangle Algorithm
+
+즉 그릴 수 있는 가장 작은 box를 Object 윤곽에 두르는 알고리즘이다.
+
+주요 개념은 방향이 dir이고 A점을 통과하는 직선과 방향이 dir에 수직이고 B점을 통과하는 직선의 교점을 반환한다는 것
+
+![mragif](https://blog.kakaocdn.net/dn/zmuGo/btq0do1k1T3/kEJd8P5vLknmTeVc2hHV70/img.gif)
--- common/featureFunctions.js
+++ common/featureFunctions.js
@@ -1,3 +1,7 @@
+if (typeof geometry === "undefined") {
+   geometry = require("./geometry.js");
+}
+
 const featureFunctions = {};
 
 featureFunctions.getPathCount = (paths) => {
@@ -25,11 +29,19 @@ featureFunctions.getHeight = (paths) => {
    return max - min;
 };
 
+featureFunctions.getElongation = (paths) => {
+   const points = paths.flat();
+   const { width, height } = geometry.minimumBoundingBox({ points });
+   return ( Math.max(width, height) + 1) / 
+          ( Math.min(width, height) + 1);
+};
+
 featureFunctions.inUse = [
    //{name:"Path Count",function:featureFunctions.getPathCount},
    //{name:"Point Count",function:featureFunctions.getPointCount},
    { name: "Width", function: featureFunctions.getWidth },
    { name: "Height", function: featureFunctions.getHeight },
+   { name: "Elongation", function: featureFunctions.getElongation },
 ];
 
 if (typeof module !== "undefined") {
