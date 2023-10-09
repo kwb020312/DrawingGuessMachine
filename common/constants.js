01054d6e67d2fbb8069e12f1ff24f375153a8d76
김우빈 <kwb020312@naver.com>
2023-10-09 18:54:05 +0900

    Feat: Show Neural Network

--- common/constants.js
+++ common/constants.js
@@ -16,7 +16,9 @@ constants.SAMPLES_JS = constants.JS_OBJECTS + "/samples.js";
 constants.TRAINING_JS = constants.JS_OBJECTS + "/training.js";
 constants.TESTING_JS = constants.JS_OBJECTS + "/testing.js";
 constants.MIN_MAX_JS = constants.JS_OBJECTS + "/minMax.js";
+constants.MODEL_JS = constants.JS_OBJECTS + "/model.js";
 constants.DECISION_BOUNDARY = constants.MODELS_DIR + "/decision_boundary.png";
+constants.MODEL = constants.MODELS_DIR + "/model.json";
 
 if (typeof module !== "undefined") {
    module.exports = constants;
